bd7495ef6fd838dcc4c3d3c1af9d0a0401a1b1d6
mrmark1998 <mrmark1998@gmail.com>
2022-10-21 19:19:50 -0400

    modified:   1/end.html 	modified:   1/end.js 	deleted:    1/package-lock.json 	deleted:    1/package.json 	modified:   2/end.html 	modified:   2/end.js 	modified:   3/end.html 	modified:   3/end.js 	deleted:    README.md 	modified:   Test/end.html 	modified:   Test/end.js 	new file:   anime/cats.png 	new file:   anime/charmander.jpg 	new file:   anime/chihiro.png 	new file:   anime/eevee.png 	new file:   anime/ghibli.jpg 	new file:   anime/kakashi.jpg 	new file:   anime/naruto.jpg 	new file:   anime/nausicaa.png 	new file:   anime/noface.png 	new file:   anime/onesies.jpg 	new file:   anime/pikachu.jpg 	new file:   anime/sakura.jpg 	new file:   anime/sasuke.png 	new file:   anime/squirtle.jpg 	new file:   anime/togepi.jpg 	new file:   anime/totoro.png 	renamed:    Example.PNG -> example1.PNG 	modified:   package.json 	modified:   style.css

--- 3/end.js
+++ 3/end.js
@@ -9,11 +9,24 @@ const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
-if(mostRecentScore >= 900) {
+//confetti for Scores >=800
+if(mostRecentScore >= 800) {
     var newScript = document.createElement('script');
     newScript.type = 'text/javascript';
     newScript.src = '../confetti.js';
     document.getElementsByTagName('head')[0].appendChild(newScript);
+    if (mostRecentScore <1000) finalScore.innerText += "\rGood Job!"
+}
+
+//anime character for perfect score
+if(mostRecentScore >= 1000) {
+    let anime = ['cats.png', 'charmander.jpg', 'chihiro.png', 'eevee.png', 'ghibli.jpg', 
+                'kakashi.jpg', 'naruto.jpg', 'nausicaa.png', 'noface.png', 'onesies.jpg', 'pikachu.jpg', 
+                'sakura.jpg', 'sasuke.png', 'squirtle.jpg', 'togepi.jpg', 'totoro.png']
+    let randomIndex = Math.floor(Math.random()*anime.length)
+    document.getElementById("anime-box").innerHTML = "<img id=\"anime-image\" src=\"/anime/" + anime[randomIndex]+ "\">";
+    finalScore.innerText += "\rYou Rock!"
+    
 }
 
 username.addEventListener('keyup', () => {
